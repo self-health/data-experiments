51385bcbad94ea275dad1ca832a4e3ac8c129919
lorgar81 <j.caleb.goodwin@gmail.com>
2018-08-27 04:13:50 -0700

    Added clean_directory script

--- pull_files.py
+++ pull_files.py
@@ -2,52 +2,56 @@ import os
 import argparse
 import sys
 import json
+import time
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import io
+from datetime import datetime
 
 # This simply checks the data folder for needed files. If they are not present it downloads them.
 # The force flag will replace all files.
 
-parser = argparse.ArgumentParser()
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
 
-parser.add_argument("--dir", help="Directory with data files", type=str, nargs='?', const=1, default='./')
-parser.add_argument("--mode", help="Select operation. Force = Replace all files. Update = Fetch files you don't have.", type=str, nargs='?', const=1, default='update')
-
-args = parser.parse_args()
+def download_file(file):
+    if os.path.exists(file['title']):
+        os.remove(file['title'])
+    new_file = drive.CreateFile({'id': file['id']})
+    new_file.GetContentFile(file['title'])
 
 if not os.path.exists('settings.yaml'):
     print 'You do not have access. Email jcalebgood@gmail.com.'
     sys.exit()
 
-if os.path.exists('./file_registry.json'):
-    with open('./file_registry.json') as data_file:
-        file_registry = json.load(data_file)
+file_list = drive.ListFile({'q': '\'' + file_registry['google_drive_folder'] + '\'' + " in parents and trashed=false"}).GetList()
+files = []
 
-for folder in file_registry['folders']:
-    if not os.path.exists(args.dir+folder):
-        os.mkdir(args.dir+folder)
+for file in file_list:
+    files.append(file)
 
+# Create directory structure
 
-# try:
-#     f = open('password.txt','r')
-#     password = f.read()
-#     password = password.strip()
-# except IOError:
-#     print 'You do not have access. Email jcalebgood@gmail.com.'
-#     sys.exit()
+for file in files:
+    file_name = file['title']
+    if '/' in file_name:
+        file_name = file_name.split('/')
+        file_name = file_name[0:-1]
+        for i in range(len(file_name)+1):
+            path = file_name[0:i]
+            if len(path):
+                path = '/'.join(path)
+                if not os.path.exists(path):
+                    os.mkdir(path)
 
-# if not os.path.exists(args.dir):
-#     os.mkdir(args.dir)
+# Pull files that are new or have been updated
 
-#import os
-# os.mkdir(path)
-
-# import zipfile
-# zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
-# zip_ref.extractall(directory_to_extract_to)
-# zip_ref.close()
-
-# data = data.split('\n')
-#
-# print os.path.exists(args.dir+'all_predications.txt')
-
-#print args.dir
-#print args.force
+for file in files:
+    if not os.path.exists(file['title']):
+        download_file(file)
+    else:
+        remote_time = float(file['description'])
+        disk_time = os.path.getmtime(file['title'])
+        if remote_time > disk_time:
+            download_file(file)
--- push_files.py
+++ push_files.py
@@ -2,14 +2,16 @@ import os
 import argparse
 import sys
 import json
+import time
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import io
 
-# This simply checks the data folder for needed files. If they are not present it downloads them.
-# The force flag will replace all files.
+# This uploads all files that are new or have been modified.
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--dir", help="Directory with data files", type=str, nargs='?', const=1, default='./')
-parser.add_argument("--mode", help="Select operation. Force = Replace all files. Update = Fetch files you don't have.", type=str, nargs='?', const=1, default='update')
 
 args = parser.parse_args()
 
@@ -20,34 +22,46 @@ if not os.path.exists('settings.yaml'):
 if os.path.exists('./file_registry.json'):
     with open('./file_registry.json') as data_file:
         file_registry = json.load(data_file)
+else:
+    print 'No registered files.'
+    sys.exit()
+
+# Delete data that is no longer there
 
 for folder in file_registry['folders']:
     if not os.path.exists(args.dir+folder):
-        os.mkdir(args.dir+folder)
+        file_registry['folders'].remove(args.dir+folder)
 
+for file in file_registry['files']:
+    if not os.path.exists(args.dir+file):
+        file_registry['files'].remove(file)
+        if file in file_registry['updates']:
+            del file_registry['updates']
 
-# try:
-#     f = open('password.txt','r')
-#     password = f.read()
-#     password = password.strip()
-# except IOError:
-#     print 'You do not have access. Email jcalebgood@gmail.com.'
-#     sys.exit()
+# Push files that are new or have been updated
 
-# if not os.path.exists(args.dir):
-#     os.mkdir(args.dir)
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
 
-#import os
-# os.mkdir(path)
+for file in file_registry['files']:
+    if file not in file_registry['updates']:
+        modified_time = os.path.getmtime(args.dir+file)
+        f = drive.CreateFile({"description":modified_time, "title":file,"parents": [{"kind": "drive#fileLink", "id":file_registry['google_drive_folder']}]})
+        f.SetContentFile(args.dir+file)
+        f.Upload()
+        file_registry['updates'][file]=time.time()
+    else:
+        modified_time = os.path.getmtime(args.dir+file)
+        recorded_time = file_registry['updates'][file]
+        if modified_time != recorded_time:
+            f = drive.CreateFile({"description":modified_time, "title":file,"parents": [{"kind": "drive#fileLink", "id":file_registry['google_drive_folder']}]})
+            f.SetContentFile(args.dir+file)
+            f.Upload()
+            file_registry['updates'][file]=modified_time
 
-# import zipfile
-# zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
-# zip_ref.extractall(directory_to_extract_to)
-# zip_ref.close()
+file_registry = json.dumps(file_registry)
+file_registry = unicode(file_registry, errors='replace')
 
-# data = data.split('\n')
-#
-# print os.path.exists(args.dir+'all_predications.txt')
-
-#print args.dir
-#print args.force
+with io.open('file_registry.json', 'w', encoding='utf-8') as f:
+    f.write(file_registry)
